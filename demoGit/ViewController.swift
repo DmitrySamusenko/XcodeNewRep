18ead9f0af3d1d4113ec132a7d9d0e756bc4cdf0
Dmitry Samusenko <royalitymean@gmail.com>
2023-01-19 23:44:39 +0300

    Initial Commit

---
+++ demoGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  demoGit
+//
+//  Created by Dmitry Samusenko on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
